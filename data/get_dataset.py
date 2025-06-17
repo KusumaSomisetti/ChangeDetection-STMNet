b983b6c83f304f6c46c80e2583af2a42825f9ec8
KusumaSomisetti <n200238@rguktn.ac.in>
2025-06-17 12:20:56 +0530

    Update get_dataset.py

--- data/get_dataset.py
+++ data/get_dataset.py
@@ -1,4 +1,3 @@
-
 from scipy.io import loadmat
 
 
@@ -26,22 +25,19 @@ def get_Hermiston_dataset():
     return img1, img2, gt
 
 
-def get_Bayarea_dataset():
-    data_set_before = loadmat(r'../../datasets/bayArea/mat/Bay_Area_2013.mat')['HypeRvieW']
-    data_set_after = loadmat(r'../../datasets/bayArea/mat/Bay_Area_2015.mat')['HypeRvieW']
-    ground_truth = loadmat(r'../../datasets/bayArea/mat/bayArea_gtChangesolf.mat')['HypeRvieW']
+def get_Bayarea_dataset(cfg):
+    mat = loadmat(cfg['data_root'] + 'bayArea_dataset.mat')
 
-    img1 = data_set_before.astype('float32')
-    img2 = data_set_after.astype('float32')
-    gt = ground_truth.astype('float32')
+    img1 = mat['T1'].astype('float32')           # Time 1 image
+    img2 = mat['T2'].astype('float32')           # Time 2 image
+    gt = mat['groundTruth'].astype('float32')    # Ground truth
 
     return img1, img2, gt
 
-
-def get_dataset(current_dataset):
+def get_dataset(current_dataset,cfg):
     if current_dataset == 'Farmland':
         return get_Farmland_dataset()  # Farmland(450, 140, 155), gt[0. 1.]
     elif current_dataset == 'Bayarea':
-        return get_Bayarea_dataset()  # Bayarea(600, 500, 224), gt[0. 1. 2.]
+        return get_Bayarea_dataset(cfg)  # Bayarea(600, 500, 224), gt[0. 1. 2.]
     elif current_dataset == 'Hermiston':
         return get_Hermiston_dataset()  # Hermiston(307, 241, 154), gt[0. 1.]
